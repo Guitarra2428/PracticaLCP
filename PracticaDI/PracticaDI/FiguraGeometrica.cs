1763662b2ca967392893e43bcf31e7662a7f5817
Luis Alberto Mesili <42626226+Guitarra2428@users.noreply.github.com>
2024-11-05 16:19:59 -0400

    Add files via upload

---
+++ PracticaDI/PracticaDI/FiguraGeometrica.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaLCP
+{
+    public class FiguraGeometrica
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public virtual double CalcularArea()
+        {
+            return Base * Altura;
+        }
+    } 
+    
+    public class Cuadrado : FiguraGeometrica
+    {
+        public override double CalcularArea()
+        {
+            return Base * Altura; 
+        }
+    }  
+    
+    public class Rectangulo : FiguraGeometrica
+    {
+        public override double CalcularArea()
+        {
+            return Base * Altura; 
+        }
+    }
+
+
+    //Refactorizando
+
+    public class FiguraGeometricaR
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public virtual double CalcularArea()
+        {
+            return Base * Altura;
+        }
+    }
+
+    public class CuadradoR : FiguraGeometricaR
+    {
+        public double Lado { get; set; }
+
+        public override double CalcularArea()
+        {
+            return Lado * Lado; 
+        }
+    }
+
+    public class RectanguloR : FiguraGeometricaR
+    {
+        public override double CalcularArea()
+        {
+            return Base * Altura;
+        }
+    }
+}
---
+++ PracticaDI/PracticaDI/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PracticaLCP;
+
+FiguraGeometrica figura = new FiguraGeometrica { Base = 5, Altura = 3 };
+Rectangulo rectangulo = new Rectangulo { Base = 5, Altura = 3 };
+Cuadrado cuadrado = new Cuadrado { Base = 4, Altura = 4 }; // Aquí debería ser igual
+
+Console.WriteLine($"Área de la figura: {figura.CalcularArea()}");
+Console.WriteLine($"Área del rectángulo: {rectangulo.CalcularArea()}");
+Console.WriteLine($"Área del cuadrado: {cuadrado.CalcularArea()}");
+
+//Refactorizando
+FiguraGeometricaR figuraR = new FiguraGeometricaR(); // no se debe instanciar directamente
+RectanguloR rectanguloR = new RectanguloR { Base = 5, Altura = 3 };
+CuadradoR cuadradoR = new CuadradoR { Lado = 4 };
+
+
+
+Console.WriteLine($"Área del rectángulo: {rectanguloR.CalcularArea()}");
+Console.WriteLine($"Área del cuadrado: {cuadradoR.CalcularArea()}");
